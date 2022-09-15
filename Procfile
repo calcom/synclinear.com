d689e9cfbc83c8f6b79d0d09c32b33ce0f1d7ecf
tedspare <ted.spare@gmail.com>
2022-09-15 13:33:40 -0400

    Add Procfile to migrate DB before release. Remove dev migrations.

---
+++ Procfile
@@ -0,0 +1,8 @@
+# To start the web app
+
+web: pnpm start
+
+# To migrate the database during release. Runs after build.
+
+release: pnpm release
+
--- prisma/migrations/20220914222854_add_github_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "github_repos" (
-    "id" TEXT NOT NULL,
-    "repoId" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "webhookSecret" TEXT NOT NULL,
-
-    CONSTRAINT "github_repos_pkey" PRIMARY KEY ("id")
-);
