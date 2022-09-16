12ed36f9f067df9d0ef16acf524d1ebdda1b090a
tedspare <ted.spare@gmail.com>
2022-09-16 12:38:45 -0400

    Add healthcheck endpoint to unblock release

---
+++ pages/api/health-check.ts
@@ -0,0 +1,25 @@
+import type { NextApiResponse } from "next";
+import prisma from "../../prisma";
+
+// GET /api/health-check
+export default async function handle(_, res: NextApiResponse) {
+    if (!process.env.DATABASE_URL) {
+        return res
+            .status(404)
+            .send(
+                "No database URL found. Check the DATABSE_URL environment variable."
+            );
+    }
+
+    try {
+        await prisma.$connect();
+        return res.status(200).send("Successfully connected to the database!");
+    } catch (e) {
+        return res
+            .status(503)
+            .send(
+                "Unable to connect to the database. Check your connection string."
+            );
+    }
+}
+
