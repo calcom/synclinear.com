8a17308e22aa652760b1fd96d7ec31a35ce78d7e
tedspare <ted.spare@gmail.com>
2022-09-17 12:41:11 -0400

    Remove unused env variables

--- typings/environment.d.ts
+++ typings/environment.d.ts
@@ -3,7 +3,6 @@ declare global {
         interface ProcessEnv {
             LINEAR_API_KEY: string;
             GITHUB_API_KEY: string;
-            GITHUB_WEBHOOK_SECRET: string;
 
             LINEAR_USER_ID: string;
             LINEAR_TEAM_ID: string;
@@ -12,10 +11,11 @@ declare global {
             LINEAR_CANCELED_STATE_ID: string;
             LINEAR_DONE_STATE_ID: string;
             LINEAR_TODO_STATE_ID: string;
-            LINEAR_IN_PROGRESS_STATE_ID: string;
 
             GITHUB_OWNER: string;
             GITHUB_REPO: string;
+
+            DATABASE_URL: string;
         }
     }
 }
