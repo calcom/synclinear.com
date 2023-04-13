a02588cb8d868476a7d30cef888a46de4b18b806
rijkvanzanten <rijkvanzanten@me.com>
2023-04-13 15:24:40 -0400

    Use standalone build mode

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
-    productionBrowserSourceMaps: true
+    productionBrowserSourceMaps: true,
+    output: "standalone"
 };
 
